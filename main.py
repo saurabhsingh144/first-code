4588eca14d9e817d23dc598a7f91b2139099419d
saurabhsingh144 <88964851+saurabhsingh144@users.noreply.github.com>
2021-08-15 14:32:10 +0530

    Add files via upload
    this is my first basic python language mini project defaulty calculator

---
+++ main.py
@@ -0,0 +1,19 @@
+
+a=int(input("enter the number1\n"))
+b=int(input("enter the number2\n"))
+c=input("enter the opertaor\n,*,+,-,/\n")
+if (a==56 and b==5 and c=="*"):
+    print("incoorect answer is:","45")
+elif(a==7 and b==3 and c=="+"):
+    print("the incoorect answer is\n 11")
+elif(c=="+"):
+    print("the sum of number is\n",a+b)
+elif(c=="*"):
+    print("the product of number is\n",a*b)
+elif (c =="/"):
+    print("the division number is\n",a/b)
+elif (c == "-"):
+    print("the substraction of the number is\n", a-b)
+else:
+    print("incoorect details")
+
